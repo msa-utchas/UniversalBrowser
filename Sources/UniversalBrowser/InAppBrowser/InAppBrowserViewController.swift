c6d18a3477d5e1db5b96dc673b07a22af73cc2c5
sakibul.utchas <63851422+msa-utchas@users.noreply.github.com>
2023-11-01 09:38:58 +0600

    view added

---
+++ Sources/UniversalBrowser/InAppBrowser/InAppBrowserViewController.swift
@@ -0,0 +1,55 @@
+//
+//  InAppBrowserViewController.swift
+//
+//
+//  Created by MD SAKIBUL ALAM UTCHAS_0088 on 1/11/23.
+//
+
+import UIKit
+import WebKit
+
+public class InAppBrowserViewController: UIViewController {
+    
+    public static let viewController = UIStoryboard(name: "UBStoryBoard", bundle: Bundle.module).instantiateViewController(withIdentifier: "DemoVC") as! InAppBrowserViewController
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    func setupWebView() {
+        
+        let webView = WKWebView()
+        
+
+        webView.frame = view.bounds
+        
+        
+        view.addSubview(webView)
+        
+
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        // Load a URL (replace with your desired URL)
+        if let url = URL(string: "https://www.example.com") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+    }
+    
+}
+
+
+
+
+
+    
+    
+    
+
